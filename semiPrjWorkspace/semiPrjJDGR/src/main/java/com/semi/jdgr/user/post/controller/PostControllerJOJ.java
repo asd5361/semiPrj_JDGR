fb2a4207147f39f55bbef71ae13cac7a281c0a9a
OKJIN\dhrwl <dhrwls94@naver.com>
2023-12-01 15:25:56 +0900

    no message

--- semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/post/controller/PostControllerJOJ.java
+++ semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/post/controller/PostControllerJOJ.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.semi.jdgr.post.service.PostServiceJOJ;
+import com.semi.jdgr.post.vo.PostVo;
+
 @WebServlet("/post/detail")
 public class PostControllerJOJ extends HttpServlet{
 	
@@ -15,7 +18,26 @@ public class PostControllerJOJ extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		try {
+			
+		// data
+		
+		// service
+		PostServiceJOJ ps = new PostServiceJOJ();
+		PostVo postDetailVo = ps.PostDetail(postDetailVo);
+		
+		// result
+		System.out.println(postDetailVo);
+		req.setAttribute("PostDetailVo", postDetailVo);
 		req.getRequestDispatcher("/WEB-INF/views/user/post/detail.jsp").forward(req, resp);
+		
+		}catch(Exception e) {
+			System.out.println("포스트 상세보기 실패");
+			e.printStackTrace();
+			req.setAttribute("errorMsg", "포스트 상세보기 실패");
+			req.getRequestDispatcher("/WEB-INF/views/user/common/error.jsp").forward(req, resp);
+		}
+		
 		
 	}
 	
---
+++ semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/post/controller/PostReplyControllerJOJ.java
@@ -0,0 +1,31 @@
+package com.semi.jdgr.user.post.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/post/reply")
+public class PostReplyControllerJOJ extends HttpServlet{
+	
+	// 포스트 상세보기 (화면)
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//리다이렉트 or ajax로 처리
+
+		// data
+		
+		// service
+		
+		// result
+		
+	}
+	
+	
+	
+
+}
