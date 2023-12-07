fff10cb7850f53fc34e1e7c04f21bb65d48fbe63
DESKTOP-URCCHTU\sales <llds0216@naver.com>
2023-12-08 08:14:44 +0900

    페이징 기능 완

--- semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/post/service/PostServiceLYJ.java
+++ semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/post/service/PostServiceLYJ.java
@@ -11,94 +11,94 @@ import com.semi.jdgr.post.dao.PostDaoLYJ;
 import com.semi.jdgr.util.JDBCTemplate;
 
 public class PostServiceLYJ {
-	
-	//맨 처음에 보이는 전체 리스트 조회
-	public List<PostVo> allSelectPostList(PostVo postVo ) throws Exception {
-		
-		// conn
-		Connection conn = JDBCTemplate.getConnection();
-		
-		// dao
-		PostDaoLYJ dao = new PostDaoLYJ();
-		List<PostVo> postVoList = dao.allSelectPostList(conn, postVo);
-		
-		
-		// 공감수 조회하는 쿼리문 실행
-		for (PostVo vo : postVoList) {
-//					vo = postVoList.get(0)
- 			String heartCnt = dao.getheartCnt(conn , vo.getPostNo());
-			vo.setHeartCnt(heartCnt);
-		}
-		
-		// 댓글수 조회하는 쿼리문 실행		
-		for (PostVo vo : postVoList) {
-			String replyCnt = dao.getreplyCnt(conn , vo.getPostNo());
-			vo.setReplyCnt(replyCnt);
-		}
-		
-		// close
-		JDBCTemplate.close(conn);
-		
-		return postVoList;
+   
+   //맨 처음에 보이는 전체 리스트 조회
+   public List<PostVo> allSelectPostList() throws Exception {
+      
+      // conn
+      Connection conn = JDBCTemplate.getConnection();
+      
+      // dao
+      PostDaoLYJ dao = new PostDaoLYJ();
+      List<PostVo> postVoList = dao.allSelectPostList(conn);
+      
+      
+      // 공감수 조회하는 쿼리문 실행
+      for (PostVo vo : postVoList) {
+//               vo = postVoList.get(0)
+          String heartCnt = dao.getheartCnt(conn , vo.getPostNo());
+         vo.setHeartCnt(heartCnt);
+      }
+      
+      // 댓글수 조회하는 쿼리문 실행      
+      for (PostVo vo : postVoList) {
+         String replyCnt = dao.getreplyCnt(conn , vo.getPostNo());
+         vo.setReplyCnt(replyCnt);
+      }
+      
+      // close
+      JDBCTemplate.close(conn);
+      
+      return postVoList;
 
-	}//allSelectPostList
-	
-	
+   }//allSelectPostList
+   
+   
 
-	//포스트 관리 목록 조회(관리자)
-	public List<PostVo> selectPostList(String memNick) throws Exception {
-		
-		// conn
-		Connection conn = JDBCTemplate.getConnection();
-		
-		// dao
-		PostDaoLYJ dao = new PostDaoLYJ();
-		List<PostVo> postVoList = dao.selectPostList(conn, memNick);
-		
-		
-		// 공감수 조회하는 쿼리문 실행
-		for (PostVo vo : postVoList) {
-//			vo = postVoList.get(0)
- 			String heartCnt = dao.getheartCnt(conn , vo.getPostNo());
-			vo.setHeartCnt(heartCnt);
-		}
-		
-		// 댓글수 조회하는 쿼리문 실행		
-		for (PostVo vo : postVoList) {
-			String replyCnt = dao.getreplyCnt(conn , vo.getPostNo());
-			vo.setReplyCnt(replyCnt);
-		}
-		
-		// close
-		JDBCTemplate.close(conn);
-		
-		return postVoList;
-	
-	}//selectPostList
+   //포스트 관리 목록 조회(관리자)
+   public List<PostVo> selectPostList(String memNick) throws Exception {
+      
+      // conn
+      Connection conn = JDBCTemplate.getConnection();
+      
+      // dao
+      PostDaoLYJ dao = new PostDaoLYJ();
+      List<PostVo> postVoList = dao.selectPostList(conn, memNick);
+      
+      
+      // 공감수 조회하는 쿼리문 실행
+      for (PostVo vo : postVoList) {
+//         vo = postVoList.get(0)
+          String heartCnt = dao.getheartCnt(conn , vo.getPostNo());
+         vo.setHeartCnt(heartCnt);
+      }
+      
+      // 댓글수 조회하는 쿼리문 실행      
+      for (PostVo vo : postVoList) {
+         String replyCnt = dao.getreplyCnt(conn , vo.getPostNo());
+         vo.setReplyCnt(replyCnt);
+      }
+      
+      // close
+      JDBCTemplate.close(conn);
+      
+      return postVoList;
+   
+   }//selectPostList
 
 
-	// 게시글 갯수 조회(맨 처음에 보이는 전체 리스트 조회)
-	public int selectPostCount() throws Exception {
-		
-		// conn
-		Connection conn = JDBCTemplate.getConnection();
-		
-		// DAO
-		PostDaoLYJ dao = new PostDaoLYJ();
-		int cnt = dao.selectPostCount(conn);
-		
-		// close
-		JDBCTemplate.close(conn);
-		
-		return cnt;
-	}
+   // 게시글 갯수 조회(맨 처음에 보이는 전체 리스트 조회)
+   public int selectPostCount() throws Exception {
+      
+      // conn
+      Connection conn = JDBCTemplate.getConnection();
+      
+      // DAO
+      PostDaoLYJ dao = new PostDaoLYJ();
+      int cnt = dao.selectPostCount(conn);
+      
+      // close
+      JDBCTemplate.close(conn);
+      
+      return cnt;
+   }
 
-	
+   
 
-	
-	
-	
-	
-	
+   
+   
+   
+   
+   
 
 }//class
