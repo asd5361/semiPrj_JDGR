8545d40067187ac901452c6f36e3c55862ceae2d
GWANGHUI\user <canach100@gmail.com>
2023-12-11 17:47:30 +0900

    신고하기

--- semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/blame/controller/ReplyBlameController.java
+++ semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/blame/controller/ReplyBlameController.java
@@ -1,6 +1,7 @@
 package com.semi.jdgr.user.blame.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import com.semi.jdgr.user.blame.service.ReplyBlameService;
 import com.semi.jdgr.user.blame.vo.ReplyBlameVo;
+import com.semi.jdgr.user.reply.vo.ReplyVo;
 
 @WebServlet("/user/blame/r_blamepop")
 public class ReplyBlameController extends HttpServlet{
@@ -20,34 +22,41 @@ public class ReplyBlameController extends HttpServlet{
 	//신고하기 모달 화면
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/WEB-INF/views/user/blame/r_blamepop.jsp").forward(req, resp);
 		try {
 			//data
-			String replyNo = req.getParameter("replyNo");
-			String postNo = req.getParameter("postNo");
-			String replyMem = req.getParameter("replyMem");
-			String parentsNo = req.getParameter("parentsNo");
-			String con = req.getParameter("con");
+			ReplyVo vo = new ReplyVo();
 			
-			ReplyBlameVo vo = new ReplyBlameVo();
-			vo.setReplyNo(replyNo);
-			vo.setPostNo(postNo);
-			vo.setReplyMem(replyMem);
-			vo.setParentsNo(parentsNo);
-			vo.setCon(con);
+			vo.setReplyNo("3");	
+			vo.setReplyMem("2");
+			vo.setCon("내용");
+
+//			String replyNo = req.getParameter("replyNo");
+//			String postNo = req.getParameter("postNo");
+//			String replyMem = req.getParameter("replyMem");
+//			String parentsNo = req.getParameter("parentsNo");
+//			String con = req.getParameter("con");
+//			
+//			ReplyBlameVo vo = new ReplyBlameVo();
+//			vo.setReplyNo(replyNo);
+//			vo.setPostNo(postNo);
+//			vo.setReplyMem(replyMem);
+//			vo.setParentsNo(parentsNo);
+//			vo.setCon(con);
 			
 			//service
 			ReplyBlameService rbs = new ReplyBlameService();
-			ReplyBlameVo rvo = rbs.getReplyInfo(vo);
-			
-			HttpSession session = req.getSession();
+			List<ReplyBlameVo> rvo = rbs.blameList();
 					
+			
 			//result(==view)
 			if(rvo == null) {
-				session.setAttribute("alertMsg", "댓글 정보 불러오기 실패");
+				req.setAttribute("alertMsg", "댓글 정보 불러오기 실패");
 				throw new Exception("댓글 신고 실패");
 			}
-			session.setAttribute("rvo", rvo);
+			req.setAttribute("rvo", rvo);
+			req.setAttribute("vo", vo);
+			req.getRequestDispatcher("/WEB-INF/views/user/blame/r_blamepop.jsp").forward(req, resp);
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 			req.setAttribute("errorMsg", "신고");
@@ -63,30 +72,20 @@ public class ReplyBlameController extends HttpServlet{
 
 		try {
 			//data
-			String rBlaNo = req.getParameter("rBlaNo");
-			String rNo = req.getParameter("rNo");
-			String rBlamerNo = req.getParameter("rBlamerNo");
+
+			
+			String rBlaList = req.getParameter("rBlaList");
+			String rBlaDetail = req.getParameter("rBlaDetail");
 			String rWriterNo = req.getParameter("rWriterNo");
 			String rBlaCon = req.getParameter("rBlaCon");
-			String rBlaDate = req.getParameter("rBlaDate");
-			String rBlaList = req.getParameter("rBlaList");
-			String rSancYn = req.getParameter("rSancYn");
-			String rAnsDate = req.getParameter("rAnsDate");
-			String rBlaDetail = req.getParameter("rBlaDetail");
-			String rDelYn = req.getParameter("rDelYn");
+			String rBlaNo = req.getParameter("rBlaNo");
 			
 			ReplyBlameVo vo = new ReplyBlameVo();
-			vo.setrBlaNo(rBlaNo);
-			vo.setrNo(rNo);
-			vo.setrBlamerNo(rBlamerNo);
+			vo.setrBlaList(rBlaList);
+			vo.setrBlaDetail(rBlaDetail);
 			vo.setrWriterNo(rWriterNo);
 			vo.setrBlaCon(rBlaCon);
-			vo.setrBlaDate(rBlaDate);
-			vo.setrBlaList(rBlaList);
-			vo.setrSancYn(rSancYn);
-			vo.setrAnsDate(rAnsDate);
-			vo.setrBlaDetail(rBlaDetail);
-			vo.setrDelYn(rDelYn);
+			vo.setrBlaNo(rBlaNo);
 			
 			
 			//service
@@ -94,13 +93,10 @@ public class ReplyBlameController extends HttpServlet{
 			int result = rbs.blame(vo);
 			
 			//result(==view)
-			if(result == 1) {
-				HttpSession session = req.getSession();
-				session.setAttribute("alertMsg", "신고가 접수됐습니다.");
-				resp.sendRedirect("/jdgr/home");
-			}else {
+			if(result != 1) {
 				throw new Exception("result 값이 1이 아님");
 			}
+			resp.sendRedirect("/jdgr/home");
 			
 		}catch(Exception e) {
 			System.out.println("신고 중 에러 발생");
