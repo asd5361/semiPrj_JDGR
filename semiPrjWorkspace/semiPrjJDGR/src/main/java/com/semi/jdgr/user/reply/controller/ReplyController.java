3808bcb4a90f104aac6696a84b4ad15109341c29
DESKTOP-65C7M8P\user1 <clsrnclsrn1111@naver.com>
2023-12-12 11:24:49 +0900

    [기능구현] 댓글 불러오기

--- semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/reply/controller/ReplyController.java
+++ semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/user/reply/controller/ReplyController.java
@@ -14,7 +14,7 @@ import com.google.gson.Gson;
 import com.semi.jdgr.user.reply.service.ReplyService;
 import com.semi.jdgr.user.reply.vo.ReplyVo;
 
-@WebServlet("/reply")
+@WebServlet("/post/reply")
 public class ReplyController extends HttpServlet{
 
 	// 댓글 목록 조회하여 JSON 형태로 문자열 내보내기
@@ -22,19 +22,13 @@ public class ReplyController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
 			// data
-			String no = req.getParameter("no");	//refNo 에 해당하는 게시글 번호를 가져옴
+			String postNo = req.getParameter("postNo");	// 포스트 넘버 가져오기
 			
 			// service
 			ReplyService bs = new ReplyService();
-			List<ReplyVo> replyVoList = bs.getReplyListByNo(no);
+			List<ReplyVo> replyVoList = bs.getReplyListByNo(postNo);
 			
-			// result (replyVoList 를 JSON 문자열로 바꿔서 내보내기 - GSON 활용 예정)
-			Gson gson = new Gson();				// GSON 이 제공하는 메소드를 사용하기 위해 객체 생성
-			String str = gson.toJson(replyVoList);	// 객체 -> JSON형식문자열 변환 함수 호출
-			
-			resp.setCharacterEncoding("UTF-8");	// 한글 처리
-			PrintWriter out = resp.getWriter();	// 문자열 내보내기를 위한 통로 준비 (JSP 필요없음)
-			out.write(str);						// JSON 으로 변환된 문자열을 내보냄
+			// result
 			
 		}catch(Exception e) {
 			System.out.println("댓글 불러오기 실패 ...");
