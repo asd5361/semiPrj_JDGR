5b7e10d7be14693f5bfd02ee2314518531c9b6e7
Deokgi Yang <corey5361@nate.com>
2023-12-13 00:01:54 +0900

    추가 - 빠짐

--- semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/userManagement/service/UserManagementService.java
+++ semiPrjWorkspace/semiPrjJDGR/src/main/java/com/semi/jdgr/userManagement/service/UserManagementService.java
@@ -13,96 +13,96 @@ import com.semi.jdgr.util.JDBCTemplate;
 public class UserManagementService {
 
 	public int selectAdminUserManagementCount() throws Exception {
-		
+
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		//dao
 		UserManagementDao dao = new UserManagementDao();
 		int cnt = dao.selectAdminUserManagementCount(conn);
-		
+
 		//close
 		JDBCTemplate.close(conn);
-		
+
 		return cnt;
 	}
 
 	public List<MemberVo> selectAdminUserManagementList(PageVo pvo) throws Exception {
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		//dao
 		UserManagementDao dao = new UserManagementDao();
 		List<MemberVo> UserManagementVoList = dao.selectAdminUserManagementListt(conn,pvo);
-		
+
 		//close
 		JDBCTemplate.close(conn);
-		
+
 		return UserManagementVoList;
 	}
-	
+
 	//회원 상세 조회
 	public MemberVo userManagementDetail(String umNo) throws Exception  {
-		
+
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		//dao
 		UserManagementDao dao = new UserManagementDao();
 		MemberVo vo = dao.userManagementDetail(conn,umNo);
-		
+
 		//close
 		JDBCTemplate.close(conn);
-		
+
 		return vo;
 	}
 	//검색 총갯스
 	public int adminUserManagementSearchCount(MemberVo searchVo) throws Exception {
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		//dao
 		UserManagementDao dao = new UserManagementDao();
 		int cnt = dao.adminUserManagementSearchCount(conn,searchVo);
-		
+
 		//close
 		JDBCTemplate.close(conn);
-		
+
 		return cnt;
 	}
 
 	public List<MemberVo> adminUserManagementeSearch(MemberVo searchVo, PageVo pvo) throws Exception {
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		//dao
 		UserManagementDao dao = new UserManagementDao();
 		List<MemberVo> userManagementVoList = dao.adminUserManagementeSearch(conn,searchVo, pvo);
-		
+
 		//close
 		JDBCTemplate.close(conn);
-		
+
 		return userManagementVoList;
 	}
 
 	public int userManagementUpdate(MemberVo vo) throws Exception {
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
-		
+
 		//dao
 		UserManagementDao dao = new UserManagementDao();
 		int result = dao.userManagementUpdate(conn,vo);
-		
+
 		//tx
 		if(result == 1) {
 			JDBCTemplate.commit(conn);
 		}else {
 			JDBCTemplate.rollback(conn);
 		}
-		
+
 		//close
 		JDBCTemplate.close(conn);
-		
+
 		return result;
 	}
 
